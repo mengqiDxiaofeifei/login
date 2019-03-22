24a935f6064e685dc68a8a505deceaabdde368d4
zhj <m17320406596@163.com>
2019-03-22 16:37:56 +0800

    first commit

---
+++ src/main/java/com/mengqid/feifei/FeifeiApplication.java
@@ -0,0 +1,13 @@
+package com.mengqid.feifei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeifeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeifeiApplication.class, args);
+    }
+
+}
