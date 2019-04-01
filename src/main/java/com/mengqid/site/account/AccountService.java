fc47ba40eb299cc63ed66620b5de40c28f3e984f
zhj <m17320406596@163.com>
2019-04-01 18:53:41 +0800

    用户相关

--- src/main/java/com/mengqid/site/account/AccountService.java
+++ src/main/java/com/mengqid/site/account/AccountService.java
@@ -31,42 +31,42 @@ public class AccountService {
     }
 
     public Response saveAccount(User user) {
-        if(!CheckUtil.isEmpty(user)){
+        if (!CheckUtil.isEmpty(user)) {
             User exsitUser = userMapper.selectByPrimaryKey(user);
-            if(!CheckUtil.isEmpty(user.getId()) && !CheckUtil.isEmpty(exsitUser)){
+            if (!CheckUtil.isEmpty(user.getId()) && !CheckUtil.isEmpty(exsitUser)) {
                 //修改
                 user.setUpdateTime(new Date());
                 userMapper.updateByPrimaryKeySelective(user);
-            }else {
+            } else {
                 //新增
-                PasswordEncoderUtil.passwordEncoder(user.getPassword());
+                user.setPassword(PasswordEncoderUtil.passwordEncoder(user.getPassword()));
                 user.setUser_uuid(ShortUUID.generate());
                 user.setStatus(1);
                 user.setCreateTime(new Date());
                 user.setUpdateTime(new Date());
                 userMapper.insert(user);
             }
-            return new Response(200,null,"成功");
-        }else {
-           return new Response(400,null,"请求参数不能为空！");
+            return new Response(200, null, "成功");
+        } else {
+            return new Response(400, null, "请求参数不能为空！");
         }
     }
 
     public Response updateStatusByids(List<Integer> ids, Integer status) {
-        if(!CheckUtil.isEmpty(ids) && ids.size() != 0){
-            ids.forEach(i -> userMapper.updateStatusById(i,status));
-            return new Response(200,null,"成功");
-        }else {
-            return new Response(400,null,"请求参数不能为空！");
+        if (!CheckUtil.isEmpty(ids) && ids.size() != 0) {
+            ids.forEach(i -> userMapper.updateStatusById(i, status));
+            return new Response(200, null, "成功");
+        } else {
+            return new Response(400, null, "请求参数不能为空！");
         }
     }
 
     public Response findUserById(Integer id) {
-        if(!CheckUtil.isEmpty(id)){
+        if (!CheckUtil.isEmpty(id)) {
             User user = userMapper.selectByPrimaryKey(id);
-            return new Response(200,user,"成功");
-        }else {
-            return new Response(400,null,"请求参数不能为空！");
+            return new Response(200, user, "成功");
+        } else {
+            return new Response(400, null, "请求参数不能为空！");
         }
     }
 }
